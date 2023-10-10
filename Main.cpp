bebdd4a3c61720f2b4af980ec71ae7d3f008f713
Timmer, Daan <daan.timmer@philips.com>
2023-10-10 10:35:08 +0000

    <html> <head><title>301 Moved Permanently</title></head> <body> <center><h1>301 Moved Permanently</h1></center> </body> </html>

--- Main.cpp
+++ Main.cpp
@@ -11,61 +11,15 @@ namespace
 
     struct Tile
     {
-        struct Empty;
-        struct W;
-        struct B;
-
-        struct Entry
+        void InitW()
         {
-            virtual void AssignW(std::variant<Empty, W, B>&)
-            {
-                throw -1;
-            }
+            entry.emplace<W>();
+        }
 
-            virtual void AssignB(std::variant<Empty, W, B>&)
-            {
-                throw -1;
-            }
-
-            virtual void Print()
-            {
-                throw -1;
-            }
-        };
-
-        struct Empty : Entry
+        void InitB()
         {
-            void AssignW(std::variant<Empty, W, B>& entry) override
-            {
-                entry.emplace<W>();
-            }
-
-            void AssignB(std::variant<Empty, W, B>& entry) override
-            {
-                entry.emplace<B>();
-            }
-
-            void Print() override
-            {
-                std::cout << ".";
-            }
-        };
-
-        struct W : Entry
-        {
-            void Print() override
-            {
-                std::cout << "W";
-            }
-        };
-
-        struct B : Entry
-        {
-            void Print() override
-            {
-                std::cout << "B";
-            }
-        };
+            entry.emplace<B>();
+        }
 
         void AssignW()
         {
@@ -85,6 +39,15 @@ namespace
                 entry);
         }
 
+        void AssignAvailable()
+        {
+            std::visit([this](auto& entry)
+                {
+                    entry.AssignAvailable(this->entry);
+                },
+                entry);
+        }
+
         void Print()
         {
             std::visit([](auto& entry)
@@ -95,40 +58,195 @@ namespace
         }
 
     private:
-        std::variant<Empty, W, B> entry;
+        struct Empty;
+        struct Available;
+        struct W;
+        struct B;
+
+        using TileVariant = std::variant<Empty, Available, W, B>;
+
+        struct Entry
+        {
+            virtual void AssignW(TileVariant&)
+            {
+                throw -1;
+            }
+
+            virtual void AssignB(TileVariant&)
+            {
+                throw -1;
+            }
+
+            virtual void AssignAvailable(TileVariant&)
+            {
+                throw -1;
+            }
+
+            virtual void Print()
+            {
+                throw -1;
+            }
+        };
+
+        struct Empty : Entry
+        {
+            void AssignAvailable(TileVariant& entry) override
+            {
+                entry.emplace<Available>();
+            }
+
+            void Print() override
+            {
+                std::cout << ".";
+            }
+        };
+
+        struct Available : Empty
+        {
+            void AssignW(TileVariant& entry) override
+            {
+                entry.emplace<W>();
+            }
+
+            void AssignB(TileVariant& entry) override
+            {
+                entry.emplace<B>();
+            }
+
+            void AssignAvailable(TileVariant&) override
+            {
+            }
+        };
+
+        struct W : Entry
+        {
+            void AssignAvailable(TileVariant&) override
+            {
+            }
+
+            void Print() override
+            {
+                std::cout << "W";
+            }
+        };
+
+        struct B : Entry
+        {
+            void AssignAvailable(TileVariant&) override
+            {
+            }
+
+            void Print() override
+            {
+                std::cout << "B";
+            }
+        };
+
+        TileVariant entry;
     };
 
     struct Board
     {
+        void AssignAvailableOrIgnore(int column, int row)
+        {
+            std::array<std::function<void(int, int)>, 2> assignOrIgnore = {
+                [](auto, auto) {},
+                [this](auto column, auto row)
+                {
+                    rows.at(row).at(column).AssignAvailable();
+                }
+            };
+
+            bool columnValid = column >= 0 && column < (width);
+            bool rowValid = row >= 0 && row < (height);
+
+            assignOrIgnore[static_cast<int>(columnValid && rowValid)](column, row);
+        }
+
         Board()
         {
-            AssignW(3, 3);
-            AssignB(4, 3);
+            InitW(3, 3);
+            InitB(4, 3);
 
-            AssignB(3, 4);
-            AssignW(4, 4);
+            InitB(3, 4);
+            InitW(4, 4);
         }
 
         void Print()
         {
+            std::cout << "  ABCDEFGH\n";
+            auto rowNr = 0;
+
             for (auto& row : rows)
             {
+                std::cout << rowNr << " ";
                 for (auto& entry : row)
                 {
                     entry.Print();
                 }
                 std::cout << "\n";
+                ++rowNr;
             }
         }
 
         void AssignW(int x, int y)
         {
+            AssignAvailableOrIgnore(x - 1, y - 1);
+            AssignAvailableOrIgnore(x, y - 1);
+            AssignAvailableOrIgnore(x + 1, y - 1);
+
+            AssignAvailableOrIgnore(x - 1, y);
             rows.at(y).at(x).AssignW();
+            AssignAvailableOrIgnore(x + 1, y);
+
+            AssignAvailableOrIgnore(x - 1, y + 1);
+            AssignAvailableOrIgnore(x, y + 1);
+            AssignAvailableOrIgnore(x + 1, y + 1);
         }
 
         void AssignB(int x, int y)
         {
+            AssignAvailableOrIgnore(x - 1, y - 1);
+            AssignAvailableOrIgnore(x, y - 1);
+            AssignAvailableOrIgnore(x + 1, y - 1);
+
+            AssignAvailableOrIgnore(x - 1, y);
             rows.at(y).at(x).AssignB();
+            AssignAvailableOrIgnore(x + 1, y);
+
+            AssignAvailableOrIgnore(x - 1, y + 1);
+            AssignAvailableOrIgnore(x, y + 1);
+            AssignAvailableOrIgnore(x + 1, y + 1);
+        }
+
+        void InitW(int x, int y)
+        {
+            AssignAvailableOrIgnore(x - 1, y - 1);
+            AssignAvailableOrIgnore(x, y - 1);
+            AssignAvailableOrIgnore(x + 1, y - 1);
+
+            AssignAvailableOrIgnore(x - 1, y);
+            rows.at(y).at(x).InitW();
+            AssignAvailableOrIgnore(x + 1, y);
+
+            AssignAvailableOrIgnore(x - 1, y + 1);
+            AssignAvailableOrIgnore(x, y + 1);
+            AssignAvailableOrIgnore(x + 1, y + 1);
+        }
+
+        void InitB(int x, int y)
+        {
+            AssignAvailableOrIgnore(x - 1, y - 1);
+            AssignAvailableOrIgnore(x, y - 1);
+            AssignAvailableOrIgnore(x + 1, y - 1);
+
+            AssignAvailableOrIgnore(x - 1, y);
+            rows.at(y).at(x).InitB();
+            AssignAvailableOrIgnore(x + 1, y);
+
+            AssignAvailableOrIgnore(x - 1, y + 1);
+            AssignAvailableOrIgnore(x, y + 1);
+            AssignAvailableOrIgnore(x + 1, y + 1);
         }
 
         std::array<std::array<Tile, width>, height> rows;
